fa6d5a6bc3510a206db9444dc5ece283d54afbc0
sashaaladko <74147568+sashaaladko@users.noreply.github.com>
2021-06-07 20:42:03 +0300

    Add files via upload

---
+++ lab3/templates/models.py
@@ -0,0 +1,13 @@
+from django.db import models
+
+
+class Task(models.Model):
+    title = models.CharField('название', max_length=200)
+    task = models.TextField('описание')
+
+    def __str__(self):
+        return  self.title
+
+    class Meta:
+        verbose_name = 'Задача'
+        verbose_name_plural = 'Задачи'
---
+++ lab3/templates/views.py
@@ -0,0 +1,35 @@
+from django.shortcuts import render, redirect
+from .models import Task
+from .forms import TaskForm
+
+
+def index(request):
+    tasks = Task.objects.all()
+    return render(request, 'main/index.html', {'title': 'Главная страница', 'tasks': tasks})
+
+
+def about(request):
+    return render(request, 'main/about.html')
+
+
+def create(request):
+    error = ''
+    if request.method == 'POST':
+        form = TaskForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('home')
+        else:
+            error = 'Форма была неверной'
+
+    form = TaskForm()
+    context = {
+        'form': form,
+        'error': error
+    }
+    return render(request, 'main/create.html', context)
+
+
+def login(request):
+    return render(request, 'main/login.html')
+
